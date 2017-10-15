fd82cdc20cc08bfb2458898724c20e226d3969eb
Sok444 <sok_88@mail.ru>
2017-10-15 15:26:28 +0300

    set db

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+#Работа с activerecord
+
+#1. Создаем подключение к базе данных
+set :database, "sqlite3:barbershop.db"
+
+#2. Создаем сущности как класс наследуемый от  ActiveRecord::Base (клиент, заказы, )
+class Client < ActiveRecord::Base 
+end
+
 
 get '/' do
   erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
