ecbdb4904b31ab2527163f3a1c685d9d010acbaa
Sok444 <sok_88@mail.ru>
2017-10-15 15:44:37 +0300

    Add clients migration

---
+++ db/migrate/20171015123509_create_clients.rb
@@ -0,0 +1,19 @@
+class CreateClients < ActiveRecord::Migration[5.1]
+  def change
+
+  	#Создаем таблицу клинетов тут мы указываем сущность в ед. числе, а таблица создастся с именем во мн. числе.
+  	create_table :client do |t|
+  		#text - тип столбца
+  		t.text :name
+  		t.text :phone
+  		t.text :datestamp
+  		t.text :barber
+  		t.text :color
+
+  		#специальный метод который добавляет автоматически 2 доп столбца даты created_at, updated_at
+
+  		t.timestamps
+  	end
+
+  end
+end
