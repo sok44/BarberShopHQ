5f5c724955fcba87f0391c5096f224e3f47d137a
Sok444 <sok_88@mail.ru>
2017-10-17 05:24:36 +0300

    update index view

--- app.rb
+++ app.rb
@@ -17,6 +17,9 @@ class Barber < ActiveRecord::Base
 end
 
 get '/' do
-  erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+  #Синтаксис ActiveRecord
+  @barbers = Barber.all
+
+  erb :index
 end
 
